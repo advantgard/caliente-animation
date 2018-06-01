af4e8d67171b271f2b965beb469c7bb87330fb48
Jorge Alvarez <j.alvarz@hotmail.com>
2018-06-01 13:24:00 +0200

    Initial setup

---
+++ src/caliente-hero-plugin.js
@@ -0,0 +1,18 @@
+$( function () {
+
+    var pub = {};
+
+    /**
+     *  Selectors
+     */
+
+    var $interactiveClips = $('[data-interactive-clip]');
+    var $interactiveButtons = $('[data-interactive-button]');
+
+    pub.init = function () {
+
+    };
+
+    return pub;
+
+});
